09b0d08d5acfa1248e455bb9a94df7d1eec4f3d3
Mahatma Mahardhika <mahatmamahardhika200588@gmail.com>
2024-12-01 23:21:22 +0700

    fix error single quote

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -7,24 +7,36 @@ function Hero() {
         <Image
           src="/assets/images/pp.jpg"
           alt="Mahatma Mahardhika"
-          width={200}
-          height={200}
+          width="200"
+          height="200"
           className="rounded-full mx-auto"
           priority
         />
         <h1 className="text-lg font-bold">Software Engineer</h1>
         <p className="text-sm max-w-[500px] mx-auto leading-relaxed">
-          With over a decade of coding experience, I have had the privilege of solving complex problems and delivering impactful solutions. My journey in technology has led me to develop robust ERP, HRIS, and POS systems, combining technical expertise with a deep understanding of user needs.
-        </p>
-        <p className={`text-sm max-w-[500px] mx-auto leading-relaxed`}>
-          Currently, I am channeling my passion for open source by nurturing innovative projects through ProgrammingLive, where I aim to contribute tools and knowledge that empower developers and businesses alike.
+          With over a decade of coding experience, I have had the privilege of
+          solving complex problems and delivering impactful solutions. My journey
+          in technology has led me to develop robust ERP, HRIS, and POS systems,
+          combining technical expertise with a deep understanding of user needs.
         </p>
         <p className="text-sm max-w-[500px] mx-auto leading-relaxed">
-          Need help, or just want to chat? Please don't hesitate to <a href="https://linktr.ee/programinglive" className="text-blue-500 hover:text-blue-700 underline">get in touch</a>.
+          Currently, I am channeling my passion for open source by nurturing
+          innovative projects through ProgrammingLive, where I aim to contribute
+          tools and knowledge that empower developers and businesses alike.
+        </p>
+        <p className="text-sm max-w-[500px] mx-auto leading-relaxed">
+          Need help, or just want to chat? Please don&apos;t  hesitate to
+          <a
+            href="https://linktr.ee/programinglive"
+            className="text-blue-500 hover:text-blue-700 underline"
+          >
+            get in touch
+          </a>
+          .
         </p>
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
