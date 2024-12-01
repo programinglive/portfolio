e7af6b2d9d5c25a6a1abfbec7f24f61424b4d906
Mahatma Mahardhika <mahatmamahardhika200588@gmail.com>
2024-12-02 00:29:05 +0700

    fix: update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  }
   /* config options here */
 };
 
