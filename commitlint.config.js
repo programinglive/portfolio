804e63a88560a8f307b1c7c407d291ad5903e323
programinglive <mahatmamahardhika200588@gmail.com>
2025-12-05 23:43:33 +0700

    fix(security): bump Next.js to 15.4.8 and setup commiter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => message.startsWith('chore(release):')]
+}
